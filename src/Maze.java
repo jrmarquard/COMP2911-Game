26363e3edbf7090d7ae0efbe86d7efe14177b7eb
Tim Myers <timmyers95@gmail.com>
2016-05-06 15:49:20 +1000

    Created some test mazes and printed them

--- src/Maze.java
+++ src/Maze.java
@@ -74,15 +74,51 @@ public class Maze {
 		}
 	}
 	
+	public void setStart(int x, int y) {
+		this.start = new Node(x, y);
+		if (x == -1) {
+			this.start.setRight(this.getNode(0, y));
+			this.getNode(0, y).setLeft(this.start);
+		} else if (y == -1) {
+			this.start.setDown(this.getNode(x, 0));
+			this.getNode(x, 0).setUp(this.start);
+		} else if (x == this.width) {
+			this.start.setLeft(this.getNode(x - 1, y));
+			this.getNode(x - 1, y).setRight(this.start);
+		} else if (y == this.height) {
+			this.start.setUp(this.getNode(x, y - 1));
+			this.getNode(x, y - 1).setDown(this.start);
+		}
+	}
+	
+	public void setFinish(int x, int y) {
+		this.finish = new Node(x, y);
+		if (x == -1) {
+			this.finish.setRight(this.getNode(0, y));
+			this.getNode(0, y).setLeft(this.finish);
+		} else if (y == -1) {
+			this.finish.setDown(this.getNode(x, 0));
+			this.getNode(x, 0).setUp(this.finish);
+		} else if (x == this.width) {
+			this.finish.setLeft(this.getNode(x - 1, y));
+			this.getNode(x - 1, y).setRight(this.finish);
+		} else if (y == this.height) {
+			this.finish.setUp(this.getNode(x, y - 1));
+			this.getNode(x, y - 1).setDown(this.finish);
+		}
+	}
+	
 	public void printMaze() {
 	   int width = 0;
 	   int height = 0;
-	   System.out.print(" ");
+	   System.out.print("  ");
 	   while (width < this.width) {
 		   if (this.getNode(width, height).getUp() == null) {
 			   System.out.print("_ ");
-		   } else {
-			   System.out.print("  ");
+		   } else if (this.getNode(width, height).getUp() == this.getFinish()){
+			   System.out.print("F ");
+		   } else if (this.getNode(width, height).getUp() == this.getStart()){
+			   System.out.print("S ");
 		   }
 		   width++;
 	   }
@@ -92,9 +128,11 @@ public class Maze {
 	   while (height < this.height) {
 		   width = 0;
 		   if (this.getNode(width, height).getLeft() == null) {
-			   System.out.print("|");
-		   } else {
-			   System.out.print(" ");
+			   System.out.print(" |");
+		   } else if (this.getNode(width, height).getLeft() == this.getFinish()){
+			   System.out.print("F ");
+		   } else if (this.getNode(width, height).getLeft() == this.getStart()){
+			   System.out.print("S ");
 		   }
 		   
 		   while (width < this.width) {
@@ -111,9 +149,28 @@ public class Maze {
 			   
 			   width++;
 		   }
+		   if (this.getNode(width - 1, height).getRight() == this.getFinish()) {
+			   System.out.print("F");
+		   } else if (this.getNode(width - 1, height).getRight() == this.getStart()) {
+			   System.out.print("S");
+		   }
 		   
 		   System.out.print("\n");
 		   height++;
 	   }
+	   height--;
+	   width = 0;
+	   System.out.print("  ");
+	   while (width < this.width) {
+		   if (this.getNode(width, height).getDown() == this.getFinish()) {
+			   System.out.print("F ");
+		   } else if (this.getNode(width, height).getDown() == this.getStart()) {
+			   System.out.print("S ");
+		   } else {
+			   System.out.print("  ");
+		   }
+		   width++;
+	   }
+	   System.out.print("\n");
 	}
 }
