6d3b3a52f90e687903fdfde856e46698ef20f7cb
joshua Tang <joshuatang117@gmail.com>
2016-05-06 21:06:42 +1000

    fixed finishing coordinates

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -2,7 +2,7 @@
 public class MazeRunner {
 
 	public static void main (String[] args) {
-		Maze maze = new Maze(10, 8);
+		Maze maze = new Maze(10, 10);
 		maze.mazeGenerator();
 		maze.setStart(-1, 0);
 		maze.setFinish(10, 9);
