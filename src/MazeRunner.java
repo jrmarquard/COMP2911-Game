b5cdd629910bed2e693b4293bebd08c07d8d52d0
Tim Myers <timmyers95@gmail.com>
2016-05-07 12:26:51 +1000

    Added more test cases

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -7,5 +7,71 @@ public class MazeRunner {
 		maze.setStart(-1, 0);
 		maze.setFinish(10, 9);
 		maze.printMaze();
+		
+		maze = new Maze(10, 10);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(10, 9);
+		maze.printMaze();
+		
+		maze = new Maze(10, 10);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(10, 9);
+		maze.printMaze();
+		
+		maze = new Maze(10, 10);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(10, 9);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
+		
+		maze = new Maze(20, 20);
+		maze.mazeGenerator();
+		maze.setStart(-1, 0);
+		maze.setFinish(20, 19);
+		maze.printMaze();
 	}
 }
