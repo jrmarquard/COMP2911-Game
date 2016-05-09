0c3e0d30d30a9368ee358fb08d8b217291f33bb0
tyler-matheson <t_matheson@hotmail.com>
2016-05-09 19:11:29 +1000

    Implemented an auto-solve feature

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,9 +1,10 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MazeRunner {
 
-	public static void main (String[] args) throws Exception {
+	public static void main (String[] args) {
 		char move;
 		boolean endGame = false;
 		
@@ -12,25 +13,14 @@ public class MazeRunner {
 		System.out.println(System.lineSeparator() + "	    A-MAZE-ING MAZE GAME" + System.lineSeparator());
 		System.out.println("  Use WASD to move, R to re-start, Q to quit," + System.lineSeparator());
 		System.out.println("	and N to generate a new maze." + System.lineSeparator());
-		System.out.println("Please enter the width of the maze and press ENTER," + System.lineSeparator());
-		System.out.println("Followed by the height of the maze and press ENTER." + System.lineSeparator());
-		System.out.println("               Please note that" + System.lineSeparator());
-		System.out.println("The width and the height have to be greater than 1" + System.lineSeparator());
+		System.out.println("please enter the maze size (n*n) then press ENTER" + System.lineSeparator());
 		
-		int width = Integer.parseInt(input.next());
-		int height = Integer.parseInt(input.next());
+		int n = Integer.parseInt(input.next());
 		
-		if(width <= 1 || height <= 1) {
-			if(input != null) {
-				input.close();
-			}
-			throw new Exception();
-		}
-		
-		Maze maze = new Maze(width, height);
+		Maze maze = new Maze(n, n);
 		maze.mazeGenerator();
 		maze.setStart(-1, 0);
-		maze.setFinish(width, height - 1);
+		maze.setFinish(n, n-1);
 		Player player = new Player(maze.getNode(0, 0));
 		maze.printMaze(player);
 		
@@ -70,16 +60,13 @@ public class MazeRunner {
 				player = new Player(maze.getNode(0, 0));
 				maze.printMaze(player);
 			}
-			if (move == 'n'){
-				System.out.println("	Please Enter a new maze size.");
-				System.out.println("Please enter the width of the maze and press ENTER,");
-				System.out.println("Followed by the height of the maze and press ENTER.");
-				width = Integer.parseInt(input.next());
-				height = Integer.parseInt(input.next());
-				maze = new Maze(width, height);
+			if (move == 'n'){ 
+				System.out.println("	Enter a new maze size.");
+				n = Integer.parseInt(input.next());
+				maze = new Maze(n, n);
 				maze.mazeGenerator();
 				maze.setStart(-1, 0);
-				maze.setFinish(width, height - 1);
+				maze.setFinish(n, n-1);
 				player = new Player(maze.getNode(0, 0));
 				maze.printMaze(player);
 			}
@@ -87,22 +74,33 @@ public class MazeRunner {
 				endGame = true;
 				System.out.println("	Quitting. Thanks for playing!");
 			}
+			if (move == 'z'){
+				AI ai = new AI();
+				ArrayList<Node> path = ai.traverseMaze(maze, player.getPosition());
+				Node next = path.remove(0);
+				while(!path.isEmpty()){
+					next = path.remove(0);
+					player.setPosition(next);
+					maze.printMaze(player);
+					try {
+					    Thread.sleep(150);
+					} catch(InterruptedException ex) {
+					    Thread.currentThread().interrupt();
+					}
+				}
+			}
 			if (player.getPosition().equals(maze.getFinish())){
 				System.out.println("	Congratulations!!! Enter a new maze size to play again.");
-				System.out.println("Please enter the width of the maze and press ENTER,");
-				System.out.println("Followed by the height of the maze and press ENTER.");
-				width = Integer.parseInt(input.next());
-				height = Integer.parseInt(input.next());
-				maze = new Maze(width, height);
+				n = Integer.parseInt(input.next());
+				maze = new Maze(n, n);
 				maze.mazeGenerator();
 				maze.setStart(-1, 0);
-				maze.setFinish(width, height - 1);
+				maze.setFinish(n, n-1);
 				player = new Player(maze.getNode(0, 0));
 				maze.printMaze(player);
 			}
 			move = input.next().charAt(0);			
 		}
-		
 		if (input != null){
 			input.close();
 		}
